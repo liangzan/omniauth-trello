4a7c117ee6b6b66ffa2859de445da0efe1a1895d
Wong Liang Zan <zan@liangzan.net>
2012-12-18 17:52:56 +0800

    corrected the specs

--- spec/omniauth/strategies/trello_spec.rb
+++ spec/omniauth/strategies/trello_spec.rb
@@ -13,19 +13,19 @@ describe OmniAuth::Strategies::Trello do
     end
 
     it 'should have correct site' do
-      subject.options.client_options.site.should eq('https://trello.com/1/')
+      subject.options.client_options.site.should eq('https://trello.com')
     end
 
     it 'should have correct request token path' do
-      subject.options.client_options.request_token_path.should eq('OAuthGetRequestToken')
+      subject.options.client_options.request_token_path.should eq('/1/OAuthGetRequestToken')
     end
-    
+
     it 'should have correct authorize path' do
-      subject.options.client_options.authorize_path.should eq('OAuthAuthorizeToken')
+      subject.options.client_options.authorize_path.should eq('/1/OAuthAuthorizeToken')
     end
-    
+
     it 'should have correct access token url' do
-      subject.options.client_options.access_token_path.should eq('OAuthGetAccessToken')
+      subject.options.client_options.access_token_path.should eq('/1/OAuthGetAccessToken')
     end
   end
 end
